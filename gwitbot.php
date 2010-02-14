3b832abd37ca196aecebcdfd776d04cd340a8a7f
Kyle Slattery <kyle@kyleslattery.com>
2010-02-14 15:52:17 -0500

    Fix payload decoding

--- gwitbot.php
+++ gwitbot.php
@@ -10,15 +10,14 @@ require_once("twitter.php");
 $twitter = new Twitter($config['twitter']['username'], $config['twitter']['password']);
 
 // Decode payload
-$payload = json_decode($_POST['payload']);
-
-$numCommits = count($payload['commits']);
+$payload = json_decode($_GET['payload']);
+$numCommits = count($payload->commits);
 $commitsPlural = $numCommits != 1 ? "s" : "";
-$branch = str_replace("/refs/heads/", "", $payload['ref']);
+$branch = str_replace("refs/heads/", "", $payload->ref);
 
 // User is just the first commit author. Not necessarily correct, but it's the
 // really the only way to know
-$user = $payload['commits'][0]['author']['name'];
+$user = $payload->commits[0]->author->name;
 
 $message = "{$user} pushed {$numCommits} commit{$commitsPlural} to {$branch}";
 
